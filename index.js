1eccad5e669ec14b1e7f34bb07bf45caca2d7201
Chalisque <>
2021-11-20 17:18:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// This is all that it needs to do
+
+/*
+const nouns = ["Apple","Cat","Dog","Bird","Horse"]
+const adjectives = ["Red","Fluffy","Angry","Slow"]
+const ps = ["You are like a","You make me think of", "Stop being a"]
+
+const random_choice = (xs) => xs[Math.floor(Math.random()*xs.length)]
+
+console.log(`${random_choice(ps)} ${random_choice(adjectives)} ${random_choice(nouns)}.`)
+*/
